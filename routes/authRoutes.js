6d00edda7aade4912bb8a71c198328ec26f08027
minkee sung <minkeesung@minkees-MacBook-Air.local>
2018-04-26 23:44:12 -0400

    first commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,7 +12,16 @@ module.exports = app => {
 		'/auth/google/callback',
 		passport.authenticate('google'),
 		(req, res) => {
-			res.redirect('/surveys')
+			res.redirect('/')
 		}
 	)
+
+	app.get('/api/logout', (req, res) => {
+		req.logout()
+		res.redirect('/')
+	})
+
+	app.get('/api/current_user', (req, res) => {
+		res.send(req.user)
+	})
 }
